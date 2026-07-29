ba2eb0b6295ed95ab3aefc2c1e6612202124c804
simarpreet <simar24112005@gmail.com>
2026-07-29 15:03:19 +0530

    added practice set 8

---
+++ chapter 9/append.py
@@ -0,0 +1,7 @@
+st="hey simar you are amazing"
+
+f=open("myfile.txt","a")
+
+f.write(st)
+
+f.close()
---
+++ chapter 9/file_write.py
@@ -0,0 +1,7 @@
+st="hey simar you are amazing"
+
+f=open("myfile.txt","w")
+
+f.write(st)
+
+f.close()
